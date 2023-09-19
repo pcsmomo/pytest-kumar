38b796f4650c3a7d092f46b3206f713103b2fa0f
Noah Kim <dongwoo_333@hanmail.net>
2023-09-19 11:42:40 +1000

    skip and skipif

--- 01-pytest-101/py_skip_mark/test_skiptests.py
+++ 01-pytest-101/py_skip_mark/test_skiptests.py
@@ -1,3 +1,6 @@
+import pytest
+import sys
+
 const = 9 / 5
 
 
@@ -9,11 +12,19 @@ def cent_to_fah(cent=0):
 # print(cent_to_fah())
 
 
+@pytest.mark.skip(reason="Skipping for no reason specified")
 def test_case01():
     assert type(const) == float
 
 
+@pytest.mark.skipif(
+    sys.version_info > (3, 8),
+    reason="doesn't work on py version above 3.8",
+    # sys.version_info > (3, 11, 4),
+    # reason="Python version is greater than 3.11.4",
+)
 def test_case02():
+    print(sys.version_info)
     assert cent_to_fah(0) == 32
 
 
