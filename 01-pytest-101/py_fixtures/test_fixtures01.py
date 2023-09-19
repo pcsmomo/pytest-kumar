f51df2576435665d9f5c40de65b41119ed488219
Noah Kim <dongwoo_333@hanmail.net>
2023-09-19 15:02:32 +1000

    37. Fixture Basics(2/3)

--- 01-pytest-101/py_fixtures/test_fixtures01.py
+++ 01-pytest-101/py_fixtures/test_fixtures01.py
@@ -12,3 +12,13 @@ def test_getItem(setup_list):
     print(setup_list[1:3])
     assert setup_list[0] == "New York"
     assert setup_list[::2] == ["New York", "Riyadh", "Mumbai"]
+
+
+def myReverse(lst):
+    lst.reverse()
+    return lst
+
+
+def test_reverseList(setup_list):
+    assert setup_list[::-2] == ["Mumbai", "Riyadh", "New York"]
+    assert setup_list[::-1] == myReverse(setup_list)
